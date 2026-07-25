51dc131e47858a80f03978106af570ec3007bf7d
Diya <diyashah2914@gmail.com>
2026-07-26 01:13:50 +0530

    feat : Add EOF token test

--- src/lexer/lexer.test.ts
+++ src/lexer/lexer.test.ts
@@ -1,7 +1,14 @@
 import { describe, it, expect } from 'vitest';
+import { tokenize } from '../lexer/lexer.js';
 
 describe('sanity check', () => {
   it('true is true', () => {
     expect(true).toBe(true);
   });
+  it('EOF token', () => {
+    const result = tokenize("");
+    expect(result.length).toBe(1);
+    expect(result[0]?.type).toBe("EOF")
+  })
 });
+
